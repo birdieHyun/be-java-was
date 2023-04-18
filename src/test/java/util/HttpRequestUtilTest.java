86fcce0478d80c16f2559e7fedd7148f62bf5f74
birdieHyun <flaehdan@gmail.com>
2023-04-18 16:35:06 +0900

    Refactor : 로직 변경에 따른 코드 변경
    의존성 수정 예정

--- src/test/java/util/HttpRequestUtilTest.java
+++ src/test/java/util/HttpRequestUtilTest.java
@@ -5,15 +5,21 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import request.HttpRequest;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
 public class HttpRequestUtilTest {
 
     @Test
     @DisplayName("reqeustLine으로부터 method를 읽어 GET을 리턴한다.")
-    void parseHttpMethod() {
+    void parseHttpMethod() throws IOException {
         String requestLine = "GET /index.html HTTP/1.1";
 
         HttpRequest request = new HttpRequest();
-        request.setRequestLine(requestLine);
+        request.setRequestLine(requestLine, new BufferedReader(new InputStreamReader(System.in)));
 
         String method = request.getMethod();
 
@@ -23,11 +29,11 @@ public class HttpRequestUtilTest {
 
     @Test
     @DisplayName("url 분리해서 index.html을 리턴한다.")
-    void parseUrl() {
+    void parseUrl() throws IOException {
         String requestLine = "GET /index.html HTTP/1.1";
 
         HttpRequest request = new HttpRequest();
-        request.setRequestLine(requestLine);
+        request.setRequestLine(requestLine, new BufferedReader(new InputStreamReader(System.in)));
 
         String url = request.getUrl();
 
@@ -36,11 +42,11 @@ public class HttpRequestUtilTest {
 
     @Test
     @DisplayName("회원가입을 해서 쿼리 파라미터가 있을 떄, /user/create 를 올바르게 리턴한다.")
-    void parseUrlWithQueryParam() {
+    void parseUrlWithQueryParam() throws IOException {
         String requestLine = "GET /user/create?userId=first&password=password%21&name=123&email=123%40123 HTTP/1.1";
 
         HttpRequest request = new HttpRequest();
-        request.setRequestLine(requestLine);
+        request.setRequestLine(requestLine, new BufferedReader(new InputStreamReader(System.in)));
 
         String url = request.getUrl();
 
